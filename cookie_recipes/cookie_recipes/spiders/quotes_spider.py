701341c9ff4eee81604364d776c4de2adc8f43e2
Jay Goldstein <jaygoldstein6@gmail.com>
2026-07-23 17:08:11 +0200

    finish quotes spider (from tutorial)

--- cookie_recipes/cookie_recipes/spiders/quotes_spider.py
+++ cookie_recipes/cookie_recipes/spiders/quotes_spider.py
@@ -2,23 +2,34 @@ from pathlib import Path
 
 import scrapy
 
-from .data_organization import DataOrganization
+# from .data_organization import DataOrganization
 
 DATADIR = Path("webdata")
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    datadir = DataOrganization.create_data_directory(name)
+    # datadir = DataOrganization.create_data_directory(name)
 
     async def start(self):
         urls = [
             "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        self.datadir.joinpath(Path(f"quotes-{page}.html")).write_bytes(response.body)
-        self.log(f"Saved page {page}")
+        # page = response.url.split("/")[-2]
+        # self.datadir.joinpath(Path(f"quotes-{page}.html")).write_bytes(response.body)
+        # self.log(f"Saved page {page}")
+
+        quotes = response.css(".quote")
+        for quote in quotes:
+            yield {
+                "text": quote.css(".text::text").get(),
+                "author": quote.css(".author::text").get(),
+                "tags": quote.css(".tags a.tag::text").getall(),
+            }
+
+        next_page = response.css(".pager .next a").xpath("@href").get()
+        if next_page is not None:
+            yield scrapy.Request(url=response.urljoin(next_page), callback=self.parse)

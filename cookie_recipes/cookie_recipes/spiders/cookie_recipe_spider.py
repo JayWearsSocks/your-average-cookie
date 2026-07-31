70f7ff34c49a4e1dcc803744eae895d042dfa72d
Jay Goldstein <jaygoldstein6@gmail.com>
2026-07-31 14:54:13 +0200

    remove unused data organization stuff for spiders

--- cookie_recipes/cookie_recipes/spiders/cookie_recipe_spider.py
+++ cookie_recipes/cookie_recipes/spiders/cookie_recipe_spider.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from .parsing import Parsing
 
 import scrapy
--- cookie_recipes/cookie_recipes/spiders/quotes_spider.py
+++ cookie_recipes/cookie_recipes/spiders/quotes_spider.py
@@ -1,14 +1,8 @@
-from pathlib import Path
-
 import scrapy
 
-# from .data_organization import DataOrganization
-
-DATADIR = Path("webdata")
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    # datadir = DataOrganization.create_data_directory(name)
 
     async def start(self):
         urls = [
@@ -18,10 +12,6 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        # self.datadir.joinpath(Path(f"quotes-{page}.html")).write_bytes(response.body)
-        # self.log(f"Saved page {page}")
-
         quotes = response.css(".quote")
         for quote in quotes:
             yield {
